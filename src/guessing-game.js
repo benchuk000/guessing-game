4aaf148da50266f0be13f25bcab9f02f42bf1a39
olya <ola_klass@mail.ru>
2017-09-25 21:15:12 +0300

    Implemented the game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.values = null;
+        this.guessValue = null;
+        this.a = 0;
+        this.b = 0;
+    }
 
     setRange(min, max) {
-
+        this.values = (new Array(max-min)).fill(min).map((item, index) => index ? item[index - 1] + 1: item);
+        this.a = min;
+        this.b = max;
     }
 
     guess() {
+        this.guessValue = Math.round((this.a + this.b) / 2);
 
+        return this.guessValue;
     }
 
     lower() {
+        this.b = this.guessValue;
 
+        this.values = this.values.slice(0, this.values.indexOf(this.guessValue));
     }
 
     greater() {
+        this.a = this.guessValue;
 
+        this.values = this.values.slice(this.values.indexOf(this.guessValue), this.values.length);
     }
 }
 
